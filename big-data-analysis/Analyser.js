d374206c4e77771c6aba9557dc75970f90464495
dralc <dr.aloysius@gmail.com>
2020-11-02 23:26:12 +1100

    stream parsing: Adds 'most common crime per area' test

--- big-data-analysis/Analyser.js
+++ big-data-analysis/Analyser.js
@@ -8,15 +8,19 @@ export class Analyser extends Transform {
 		this._crimesPerYr_m = new Map()
 		// <borough, crimecount>
 		this._crimesPerBorough_m = new Map()
+		// <borough, <category, crimecount>>
+		this._crimesPerBorough_by_category_m = new Map()
 	}
 
 	/**
-	 * Sum up the `value` for each year and store in a map: Map<year, crimeCount>
+	 * Aggregate data into Maps
 	 * 
 	 * @param {Array} chunk A record
 	 */
 	_transform(chunk, enc, cb) {
 		const borough = chunk[1]
+		const major_category = chunk[2]
+		const minor_category = chunk[3]
 		const value = parseInt(chunk[4])
 		const year = chunk[5]
 
@@ -27,6 +31,14 @@ export class Analyser extends Transform {
 		// Collect data for 'Most dangerous areas'
 		prevVal = this._crimesPerBorough_m.get(borough) ?? 0
 		this._crimesPerBorough_m.set(borough, prevVal + value)
+
+		// Collect data for 'Most common crime per area'
+		let crimesByCat_m = this._crimesPerBorough_by_category_m.get(borough)
+		if ( !crimesByCat_m ) {
+			crimesByCat_m = new Map()
+			this._crimesPerBorough_by_category_m.set(borough, crimesByCat_m)
+		}
+		crimesByCat_m.set(`${major_category}:${minor_category}`, value)
 		
 		cb()
 	}
@@ -35,6 +47,7 @@ export class Analyser extends Transform {
 		// Aggregate data
 		this._all_m.set('crimesPerYear', this._crimesPerYr_m)
 		this._all_m.set('crimesPerBorough', this._crimesPerBorough_m)
+		this._all_m.set('crimesPerBorough_by_category', this._crimesPerBorough_by_category_m)
 		this.push(this._all_m)
 		cb()
 	}
@@ -63,17 +76,12 @@ export function didCrimeGoUp(map) {
 }
 
 /**
- * Sorts the map by descending order of values, then returns the keys
+ * Orders the entries in a map.
  * 
- * @param {Map<String, Number>} map A map of <borough, crimeCount>
- * @param {number} limit Limit the no. of returned keys
- * 
- * @returns {Array<String>} Returns the sorted keys of the map
+ * @param {Map} map 
+ * @param {string} order Can be 'asc' or 'desc'. Defaults to descending order
  */
-export function mostDangerousAreas(map, limit=10) {
-	const _limit = parseInt(limit) ?? null
-	if (!_limit) throw Error('"limit" needs to be an int.')
-
+export function sortMap(map, order='desc') {
 	const ar = Array.from(map).sort((a, b) => {
 		if (a[1] > b[1]) {
 			return -1
@@ -83,8 +91,38 @@ export function mostDangerousAreas(map, limit=10) {
 		return 0;
 	});
 
-	const sorted_m = new Map(ar)
-	const keys_a = [ ...sorted_m.keys() ]
+	return new Map(ar)
+}
+
+/**
+ * Sorts the map by descending order of values, then returns the keys
+ * 
+ * @param {Map<String, Number>} map A map of <borough, crimeCount>
+ * @param {number} limit Limit the no. of returned keys
+ * 
+ * @returns {Array<String>} Returns the sorted keys of the map
+ */
+export function mostDangerousAreas(map, limit) {
+	const _limit = !isNaN(parseInt(limit)) ? parseInt(limit) : map.size + 1
+	const keys_a = [ ...sortMap(map).keys() ]
 	
 	return keys_a.slice(0, _limit)
+}
+
+/**
+ * 
+ * @param {Map} map <borough, <category, crimecount>>
+ * @param {string} borough
+ */
+export function mostCommonCrime(map, limit) {
+	let _limit = parseInt(limit)
+	_limit = !isNaN(_limit) ? _limit : map.size + 1
+
+	for (let [borough, crimesByCat_m] of map) {
+		// Sort by biggest crime count
+		const ar = Array.from(sortMap(crimesByCat_m)).slice(0, _limit)
+		map.set(borough, new Map(ar))
+	}
+
+	return map;
 }
--- big-data-analysis/tests/test.analyser.js
+++ big-data-analysis/tests/test.analyser.js
@@ -1,7 +1,7 @@
 import test from 'ava'
 import { createReadStream } from 'fs';
 import { pipeline } from 'stream';
-import { Analyser, didCrimeGoUp, mostDangerousAreas } from '../Analyser.js';
+import { Analyser, didCrimeGoUp, mostCommonCrime, mostDangerousAreas } from '../Analyser.js';
 import { resolve } from 'path';
 import csvparse from "csv-parse";
 import { promisify } from "util";
@@ -35,13 +35,14 @@ test('Analysis: Did crime go up over the years?', t => {
 test('Analysis: What are the 10 most dangerous areas?', t => {
 	const crimesMap = aggregates.get('crimesPerBorough')
 	t.snapshot(crimesMap, 'No. of crimes')
-	t.snapshot(mostDangerousAreas(crimesMap), 'Most dangerous areas')
+	t.snapshot(mostDangerousAreas(crimesMap, 5), 'Most dangerous areas')
 })
 
-// test.skip('Analysis: What is the most common crime per area?', (t) => {
-// 	t
-// })
+test('Analysis: What is the most common crime per area?', (t) => {
+	const crimesMap = aggregates.get('crimesPerBorough_by_category')
+	t.snapshot(mostCommonCrime(crimesMap, 1), 'Most common crimes per Borough')
+})
 
-// test.skip('Analysis: What is the least common crime overall?', (t) => {
-// 	t
-// })
+test.skip('Analysis: What is the least common crime overall?', (t) => {
+	t
+})
